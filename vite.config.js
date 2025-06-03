805f98be80c2460e891e3cca4e8b04f8adf52e82
pamquale <pamquale@gmail.com>
2025-06-03 20:17:34 +0300

    Настроил деплой на GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/webClicker/',   // <— здесь указываете имя вашего репозитория
+  plugins: [react()]
 })
